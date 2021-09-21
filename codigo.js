72ff497601c5e71d83d7457a2a05875ceae25d06
Aglo <55658607+Agl0@users.noreply.github.com>
2021-09-21 14:22:16 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,10 @@
+function saludar(){
+    window.alert("OUCH! Eso duele.");
+    console.log("Has pulsado el botón");
+}
+
+function contar(l){
+    for (var i = 1; i <= l; i++) {
+        console.log(i);
+       }
+}
